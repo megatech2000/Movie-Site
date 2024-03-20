166b2f53f6ef137a362486c988e23d8469517261
megatech2000 <151417898+megatech2000@users.noreply.github.com>
2024-03-20 16:44:28 +0530

    uploading

--- src/app/myspace/page.jsx
+++ src/app/myspace/page.jsx
@@ -1,8 +1,12 @@
 const page = () => {
   return (
-    <div>
-      <div>myspace</div>
-    </div>
+    <section className="h-[100vh] flex items-center justify-center">
+      <div className="flex items-center justify-center px-10 md:px-0 text-center ">
+        <h1 className="text-6xl font-bold text-green-200">
+          Under Maintance !!!
+        </h1>
+      </div>
+    </section>
   );
 };
 
